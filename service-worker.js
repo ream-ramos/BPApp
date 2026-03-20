44c2266a245fde84cf5356f5cbed1ef4872b4431
RenzJhon <ream.ramos.ui@Phinmaed.com>
2026-03-21 00:32:12 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@ self.addEventListener("install", function(e) {
         caches.open("todo-cache").then(function(cache) {
             return cache.addAll([
                 "/index.html",
-                "/BP192.png",
+                "/bp192.png",
                 "/bp512.png"
             ]);
         })
